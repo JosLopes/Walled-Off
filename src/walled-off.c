62652c37064bc0a10d81ac5e9f4fb1f199ca8465
Sushe <jlopesum004@gmail.com>
2023-04-25 10:28:22 +0100

    character -> character's adress

--- src/walled-off.c
+++ src/walled-off.c
@@ -7,14 +7,20 @@
 #include <time.h>
 #include <stdlib.h>
 
+void init_character(Character *character)
+{
+  character -> x = 0;
+  character -> y = 0;
+}
+
 int main () {
     WINDOW *main_window;
     char map[MAP_HEIGHT][MAP_WIDTH];
     Room rooms[MAX_ROOMS];
     int number_of_non_overlaping_rooms;
     srand(time(NULL));
+    Character character;
     int numRooms = rand() % (MAX_ROOMS - 35) + 10;
-    Character character = {0, 0, "warrior", 100, 0, {{"None", 1, 1}, {"None", 1, 1}, {"None", 1, 1}}};
     int num_goblins = rand() % 10;
     int num_orcs = rand() % 10;
     int num_skeletons = rand() % 10;
@@ -26,16 +32,17 @@ int main () {
     
     main_window = create_window (MAP_HEIGHT, MAP_WIDTH, 2, 2);
 
+    init_character (&character);
     fillMap (MAP_HEIGHT, MAP_WIDTH, map);
     number_of_non_overlaping_rooms = generateRooms (MAP_HEIGHT, MAP_WIDTH, map, rooms, numRooms);
     generateCorridors (MAP_WIDTH, map, rooms, numRooms, number_of_non_overlaping_rooms);
-    place_player (MAP_HEIGHT, MAP_WIDTH, map, character);
+    place_player (MAP_HEIGHT, MAP_WIDTH, map, &character);
     place_enemies (MAP_HEIGHT, MAP_WIDTH, map, num_goblins, num_skeletons, num_orcs);
     display_map (main_window, MAP_HEIGHT, MAP_WIDTH, map);
 
     wrefresh (main_window);
 
-    movement (character, main_window);
+    movement (&character, main_window);
     keypad (main_window, TRUE);
 
     getch ();
