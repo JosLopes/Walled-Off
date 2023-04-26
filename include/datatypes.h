cf36b1c19a080982ec6a44eaaf12bb4d0e4c6a74
Sushe <jlopesum004@gmail.com>
2023-04-26 18:27:38 +0100

    Struct for enemies

--- include/datatypes.h
+++ include/datatypes.h
@@ -19,12 +19,6 @@ typedef struct
   int pathX, pathY;
 } Vector;
 
-
-typedef struct {
-    int x, y;
-    char symbol;
-} Enemy;
-
 /* Weapon parameters*/
 typedef struct {
   char name[20];
@@ -42,4 +36,22 @@ typedef struct {
   Weapon weapons[3];
 } Character;
 
+typedef struct {
+  char inteligence; /* Initial d(umb), s(mart) or g(enius) */
+  int life;
+  int range; /* Range for ranged attacks */
+  int xp_from_death; /*XP earned when the player kills the enemy */
+  int screaming_range; /* Range from where it can call allies to fight with him*/
+  int poison_level; /* Damage given by poison (long duration) */
+  int group_desire; /* These type of enemys attack in group, this tells how many enemys */
+                    /* In a group need to exist before he comes back to fight           */
+} Tag;
+
+typedef struct {
+  Tag tag; /* dumb, smart or genius (evaluates the enemys inteligence) */
+  char name[10];
+  char display_symbol; /* Map character representing the enemy */
+  int x, y; /* Position on the map */
+  int damage; /* Damage given to the player*/
+} Enemy;
 #endif
