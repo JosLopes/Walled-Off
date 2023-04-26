ed005df58ca2f42b2e4bcc9f4730d3e557ffc503
Sushe <jlopesum004@gmail.com>
2023-04-26 22:00:33 +0100

    Added more modular features

--- include/mapgen.h
+++ include/mapgen.h
@@ -5,7 +5,8 @@
 
 void fillMap(int map_height, int map_width, char map[][map_width]);
 int generateRooms(int map_height, int map_width, char map[][map_width], Room rooms[], int numRooms);
-void generateCorridors(int map_width, char map[][map_width], Room rooms[], int numRooms, int number_of_non_overlaping_rooms);
+void init_non_overlaping_rooms (Room rooms[], int numRooms, char no_overlpg[], int nor_size);
+void generateCorridors(int map_width, char map[][map_width], Non_overlaping_rooms rooms[], int nor_size);
 void place_player(int map_height, int map_width, char map[][map_width], Character *character);
 void place_enemies(int map_height, int map_width, char map[][map_width], int num_goblins, int num_skeletons, int num_orcs);
 WINDOW *create_window (int height, int width, int startingX, int startingY);
