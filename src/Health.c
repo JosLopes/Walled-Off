e7687cd47fdc9b7b1fa0465ff74ea517b0a9bc93
daniellpsilva1 <a104086@alunos.uminho.pt>
2023-04-26 18:46:28 +0100

    Add function range and detect_enemy

--- src/Health.c
+++ src/Health.c
@@ -24,13 +24,40 @@ bool isWithinRange(Enemy enemy, int charX, int charY, int range) {
   return distance <= range;
 }
 
+int range (Character *character,int map_height, int map_width, char map[][map_width], WINDOW *main_window) {
+  int x = character->x, y = character->y;
+  for (y; y < range; y++){
+    for (x; x < range; x++){
+      if (map[y][x] == ENEMY_G || map[y][x] == ENEMY_O || map[y][x] == ENEMY_S) {
+        return 1; /*true -> should receive damage*/
+      }
+    }
+  }
+  return 0; /*false -> free path*/
+}
+
+void detect_enemy (Character *character,int map_height, int map_width, char map[][map_width], WINDOW *main_window){
+  int x = character->x, y = character->y;
+  for (y; y < range; y++){
+    for (x; x < range; x++){
+      if (map[y][x] == ENEMY_G || map[y][x] == ENEMY_O || map[y][x] == ENEMY_S) {
+        return 1; /*true -> should receive damage*/
+      }
+    }
+  }
+
+}
 
 void damage (Character *character,int map_height, int map_width, char map[][map_width], WINDOW *main_window){
 
 
   if (map [character -> y][character -> x] == FIRE_CHAR)
   {
-    character -> life = character -> life - 20;
+    character -> life = character -> life - 10;
+  }
+  else if (range (character, map_height, map_width, map[][map_width], WINDOW *main_window ))
+  {
+    
   }
 }
 
