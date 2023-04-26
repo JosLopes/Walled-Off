91fd8c8cfca9aa89faacd83712d47acf83565a9f
Cerqueira025 <114937042+Cerqueira025@users.noreply.github.com>
2023-04-26 22:20:38 +0100

    Create save_game.c

---
+++ include/save_game.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <math.h>
+#include "datatypes.h"
+#include "defines.h"
+#include "movement.h"
+#include "mapgen.h"
+
+void save_game(int xp) 
+{
+  FILE* file = fopen("savegame.txt", "w");
+  if (file == NULL) 
+  {
+    printf("Error opening savegame.txt\n");
+    exit(1);
+  }
+  fprintf(file, "%d\n", xp);
+  fclose(file);
+}
+
+int load_game() 
+{
+  FILE* file = fopen("savegame.txt", "r");
+  if (file == NULL) 
+  {
+    printf("No savegame found\n");
+    return 0;
+  }
+  int xp;
+  fscanf(file, "%d", &xp);
+  fclose(file);
+  return xp;
+}
