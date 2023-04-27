ce90f9b945a5a91487a9c69f4d5e56e0ec16fdc7
Sushe <jlopesum004@gmail.com>
2023-04-27 21:52:59 +0100

    Final Demo

--- src/MOBs.c
+++ src/MOBs.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include <stdio.h>
 
-int locate_positions (int map_heigth, int map_width, char map[][map_width], int enemys_size, Enemy enemys[], int nor_size, Non_overlaping_rooms no_overlpg[])
+int locate_positions (int map_heigth, int map_width, char map[][map_width], int enemies_size, Enemy enemies[], int nor_size, Non_overlaping_rooms no_overlpg[])
 {
   srand(time(NULL)); /* initializes the seed for random numbers */
-  int enemys_ind = 0, rooms_ind;
+  int enemies_ind = 0, rooms_ind;
   int offset, impossible_location = 0;  /* Max offset from the center of a room */
 
-  if (enemys == NULL) {
+  if (enemies == NULL) {
     printf ("Not able to alloc memory.");
     return 1;
   }
@@ -17,16 +17,16 @@ int locate_positions (int map_heigth, int map_width, char map[][map_width], int 
   for (; rooms_ind < nor_size; rooms_ind ++)
   {
     offset = 1;
-    while (enemys_ind < enemys_size && impossible_location = 0)
+    while (enemies_ind < enemies_size && impossible_location = 0)
     {
       int possible_x = no_overlpg[rooms_ind].midX + (rand () % offset - rand () % offset);
       int possible_y = no_overlpg[rooms_ind].midY + (rand () % offset - rand () % offset);
 
       if (possible_x < map_width && possible_y < map_heigth && map[possible_y][possible_x] == FLOOR_CHAR)
       {
-        enemys[enemys_ind] -> x = possible_x;
-        enemys[enemys_ind] -> y = possible_y;
-        enemys_ind ++;
+        enemies[enemies_ind] -> x = possible_x;
+        enemies[enemies_ind] -> y = possible_y;
+        enemies_ind ++;
         offset ++;
       }
       else
@@ -35,32 +35,91 @@ int locate_positions (int map_heigth, int map_width, char map[][map_width], int 
       }
     }
   }
-  return enemys_ind;
+  return enemies_ind;
+}
+
+Variable_stats *d_enemies_variable_stats ()
+{
+  Variable_stats *d_variables = malloc (sizeof (Variable_stats) * D_ENEMIES);
+
+  *d_variables = {{'A',"Albertino", 0, 0, 0},
+                  {'A',"Josefino", 0, 0, 0 }};
+
+  return d_variables; 
+}
+
+Variable_stats *s_enemies_variable_stats ()
+{
+  Variable_stats *s_variables = malloc (sizeof (Variable_stats) * S_ENEMIES);
+
+  *s_variables = {{'C',"Carambolas", 0, 0, 0},
+                  {'J',"JMotherFucker", 0, 0, 0 }};
+
+  return s_variables; 
+}
+
+Variable_stats *g_enemies_variable_stats ()
+{
+  Variable_stats *g_variables = malloc (sizeof (Variable_stats) * G_ENEMIES);
+
+  *g_variables = {{'I',"Imtired", 0, 0, 0},
+                  {'G',"Godblastmetodeath", 0, 0, 0 }};
+
+  return g_variables; 
+}
+
+void init_enemy (int enemies_size, Enemies *enemies, Tag *tag, Variable_stats d_variables, Variable_stats s_variables, Variable_stats d_variables)
+{
+  int current_enemy = 0;
+  int dumb_enemies = number_of_enemies * 0.6;
+  int dumb_plus_smart = dumb_enemies + (number_of_enemies * 0.3); 
+
+  for (; current_enemy < dumb_enemies; current_enemy ++)
+  {
+    init_tag (tag[current_enemy], D_CHAR, D_MAX_XP, D_MIN_XP, D_SCREAM_RANGE, D_POISON, D_GROUP_DESIRE);
+    init_enemy_stats (enemy[current_enemy], tag[current_enemy], d_variables[rand() % D_ENEMIES]);
+  }
+  free (d_variables);
+
+  for (; current_enemy < dumb_plus_smart; current_enemy ++)
+  {
+    init_tag (tag[current_enemy], S_CHAR, S_MAX_XP, S_MIN_XP, S_SCREAM_RANGE, S_POISON, S_GROUP_DESIRE);
+    init_enemy_stats (enemy[current_enemy], tag[current_enemy], s_variables[rand() % S_ENEMIES]);
+  }
+  free (s_variables);
+
+  for (; current_enemy < enemies_size ; current_enemy ++)
+  {
+    init_tag (tag[current_enemy], G_CHAR, G_MAX_XP, G_MIN_XP, G_SCREAM_RANGE, G_POISON, G_GROUP_DESIRE);
+    init_enemy_stats (enemy[current_enemy], tag[current_enemy], g_variables[rand() % G_ENEMIES]);
+  }
+  free (g_variables);
 }
 
 void init_tag (Tag *tag, char intel, char life, int range, int max_xp, int min_xp, int scream_range, int poison, int group_desire)
 {
   tag -> inteligence = intel /* Initial for "tag" */
-  tag -> life = life; /* Enemies life */
-  tag -> range = range; /* Range for distance attacks */
   tag -> xp_from_death = (rand() % max_xp-min_xp) + min_xp; /* Xp gained from killing this enemy type */
   tag -> screaming_range = scream_range;
   tag -> poison_level = poison;  /* Gives information about how long does it last, the higher the */
                                  /* Number, the more steps the player needs to take to decipate   */
                                  /* The poison and stop taking damage                             */
+  tag -> group_desire = group_desire;
 }
 
-void init_enemy (Enemy *enemy, Tag tag, char name[], char symbol, damage)
+void init_enemy_stats ()
 {
   enemy -> tag = tag;
+  enemy -> display = variables -> display /* Character to represent the enemy in the map */
 
   /* Copies the string "name" to the respective struct */
   for (int name_ind = 0; name_ind < 9 && enemy -> name[name_ind] != "\0"; name_ind ++)
   {
-    enemy -> name[name_ind] = name[name_ind];
+    enemy -> name[name_ind] = variables -> name[name_ind];
   }
   enemy -> name[name_ind] = "\0";
 
-  enemy -> display_symbol = symbol; /* Character to represent the enemy in the map */
-  enemy -> damage = damage;
+  enemy -> life = variables -> life;
+  enemy -> range = variables -> range;
+  enemy -> damage = variables -> damage;
 }
