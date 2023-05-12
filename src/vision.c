fe4555f6a29b7985b5934e43a45e5815c132a75f
a104188 <a104188@alunos.uminho.pt>
2023-05-12 23:31:01 +0100

    raio de visão corrigido

--- src/vision.c
+++ src/vision.c
@@ -42,33 +42,36 @@ int sets_range (int life){
 void vision_color (WINDOW *main_window, Character *character, char **map, int map_width ,char traveled_path[][map_width])
 {
   int range = sets_range(character->life);
+
   /*defines x_min, x_max, y_min and y_max according to the range*/
   int x, y;
-  int x_min = fmax(character->x - range+1, 0), x_max = fmin(character->x + range-1, MAP_WIDTH - 1);
-  int y_min = fmax(character->y - range+1, 0), y_max = fmin(character->y + range-1, MAP_HEIGHT - 1);
-      
+
   for (x = 0; x < MAP_WIDTH; x++) {
     for (y = 0; y < MAP_HEIGHT; y++) {
-
-      if (x >= x_min && y >= y_min && x <= x_max && y <= y_max)
+  
+    int dist = sqrt(pow(x - character ->x, 2) + pow(y - character->y, 2));
+    int x_min = fmax(character->x - dist, 0), x_max = fmin(character->x + dist, MAP_WIDTH - 1);
+    int y_min = fmax(character->y - dist, 0), y_max = fmin(character->y + dist, MAP_HEIGHT - 1);
+  
+      if (dist<=range && x >= x_min && y >= y_min && x <= x_max && y <= y_max)
       {
         /* add the viwed places to the list traveled_path to make they apper on the screen */
         traveled_path[y][x] = map[y][x];
       }
 
       /*case character position*/
-      if (x == character->x && y == character->y){
+      if (x == character->x && y == character->y)
+      {
         wattron(main_window, COLOR_PAIR(PLAYER_VISION_COLOR1)); 
         mvwaddch(main_window, y, x, traveled_path[y][x]); 
         wattroff(main_window, COLOR_PAIR(PLAYER_VISION_COLOR1));
       }
-      else if (traveled_path[y][x] == WALL_CHAR || traveled_path[y][x] == D_CHAR || traveled_path[y][x] == G_CHAR || traveled_path[y][x] == S_CHAR){
+      else if (traveled_path[y][x] == WALL_CHAR){
         map_colors(main_window, map_width, y, x, traveled_path);
       }
       else
       {
         /*sets the colors according to the distance to the character*/
-        int dist = sqrt(pow(x - character ->x, 2) + pow(y - character->y, 2));
         switch (dist)
         {
           case 5:
@@ -77,9 +80,9 @@ void vision_color (WINDOW *main_window, Character *character, char **map, int ma
             wattroff(main_window, COLOR_PAIR(PLAYER_VISION_COLOR4));
             break;
           case 4:
-            wattron(main_window, COLOR_PAIR(PLAYER_VISION_COLOR4)); 
+            wattron(main_window, COLOR_PAIR(PLAYER_VISION_COLOR3)); 
             mvwaddch(main_window, y, x, traveled_path[y][x]); 
-            wattroff(main_window, COLOR_PAIR(PLAYER_VISION_COLOR4));
+            wattroff(main_window, COLOR_PAIR(PLAYER_VISION_COLOR3));
             break;
           case 3:
             wattron(main_window, COLOR_PAIR(PLAYER_VISION_COLOR3));  
