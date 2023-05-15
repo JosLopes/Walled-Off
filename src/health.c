7925c65b3d4b2a928059dc998ce204621321d899
daniellpsilva1 <a104086@alunos.uminho.pt>
2023-05-15 18:35:55 +0100

    Add Function to place foods in NOR

--- src/health.c
+++ src/health.c
@@ -12,16 +12,110 @@
 #include "MOBs.h"
 #include "vision.h"
 
+
+/* Function to locate the positions available to put the consumables in the Non overlaping rooms */
+void locate_positions_consumables(int map_heigth, int map_width, char **map, Consumables *consumables, int number_of_enemies, int nor_size, Non_overlaping_rooms no_overlpg[]) {
+  int consumables_ind = 0;
+  int rooms_ind = 1;
+  int max_per_room = 0;
+  int offset;
+  int impossible_location = 0;  /* Max offset from the center of a room */
+
+  /* Use this number to determine the number of foods/potions in the map */
+  int number_of_consumables = number_of_enemies / 3;
+
+  for (; rooms_ind < nor_size; rooms_ind ++)
+  {
+    offset = 4;
+    impossible_location = 0;
+    max_per_room = 0;
+    while (consumables_ind < number_of_consumables && impossible_location == 0 && max_per_room < 2)
+    {
+      int possible_x = no_overlpg[rooms_ind].midX + (rand () % offset - rand () % offset);
+      int possible_y = no_overlpg[rooms_ind].midY + (rand () % offset - rand () % offset);
+
+      if (possible_x < map_width && possible_x > 0 && possible_y < map_heigth && possible_y > 0 && map[possible_y][possible_x] == FLOOR_CHAR)
+      {
+        consumables[consumables_ind].x = possible_x;
+        consumables[consumables_ind].y = possible_y;
+        max_per_room ++;
+        consumables_ind ++;
+        offset += 2;
+
+        if (consumables_ind % 2 == 0) {
+          /* Add FOOD_CHAR */
+          map[possible_y][possible_x] = FOOD_CHAR;
+        } else {
+          /* Add POTION_CHAR */
+          map[possible_y][possible_x] = POTION_CHAR;
+        }
+      }
+      else
+      {
+        impossible_location ++;
+      }
+    }
+  }
+}
+
+// /* Function to locate the positions available to put the consumables out of the Non overlaping rooms */
+// void locate_positions_consumables_outside(int map_heigth, int map_width, char **map, Consumables *consumables, int number_of_enemies, int nor_size, Non_overlaping_rooms no_overlpg[]) {
+//   int consumables_ind = 0;
+//   int rooms_ind = 1;
+//   int max_per_room = 0;
+//   int offset;
+//   int impossible_location = 0;  /* Max offset from the center of a room */
+
+//   /* Use this number to determine the number of foods/potions in the map */
+//   int number_of_consumables = number_of_enemies / 3;
+
+//   for (; rooms_ind < nor_size; rooms_ind ++)
+//   {
+//     offset = 4;
+//     impossible_location = 0;
+//     max_per_room = 0;
+//     while (consumables_ind < number_of_consumables && impossible_location == 0 && max_per_room < 2)
+//     {
+//       int possible_x = no_overlpg[rooms_ind].midX + (rand () % offset - rand () % offset);
+//       int possible_y = no_overlpg[rooms_ind].midY + (rand () % offset - rand () % offset);
+
+//       if (possible_x < map_width && possible_x > 0 && possible_y < map_heigth && possible_y > 0 && map[possible_y][possible_x] == FLOOR_CHAR)
+//       {
+//         consumables[consumables_ind].x = possible_x;
+//         consumables[consumables_ind].y = possible_y;
+//         max_per_room ++;
+//         consumables_ind ++;
+//         offset += 2;
+
+//         if (consumables_ind % 2 == 0) {
+//           /* Add FOOD_CHAR */
+//           map[possible_y][possible_x] = FOOD_CHAR;
+//         } else {
+//           /* Add POTION_CHAR */
+//           map[possible_y][possible_x] = POTION_CHAR;
+//         }
+//       }
+//       else
+//       {
+//         impossible_location ++;
+//       }
+//     }
+//   }
+// }
+
 /*
 * Function to spawn foods and potions in the map (WITHOUT ROOMS)
 */
-void place_foods_and_potions(char** map, int number_of_enemies)
+void place_foods_and_potions(int map_heigth, int map_width, char** map, Consumables *consumables, int number_of_enemies, int nor_size, Non_overlaping_rooms no_overlpg[])
 {
-  /* Use this random number to determine the number of foods/potions in the map (it needs to be > 5) */
+  /* Use this number to determine the number of foods/potions in the map */
   int number_of_consumables = number_of_enemies / 3;
 
-  /* Loop that works between 0 and number of consumables */
-  for (int i = 0; i < number_of_consumables; i++)
+  /* Put half of the foods in the map (Non overlaping rooms)*/
+  locate_positions_consumables(map_heigth, map_width, map, consumables, number_of_enemies, nor_size, no_overlpg);
+
+  /* Put the other half of the foods in the map (Out of the Non overlaping rooms) */
+  for (int i = 0; i < number_of_consumables / 2; i++)
   {
     int x;
     int y;
