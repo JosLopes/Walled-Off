414f473e58e2a5570feb78c8ec5de1c2cda4506c
daniellpsilva1 <a104086@alunos.uminho.pt>
2023-05-15 18:37:40 +0100

    Add Function to place foods in NOR 2nd step

--- include/health.h
+++ include/health.h
@@ -3,8 +3,8 @@
 #include "defines.h"
 #ifndef HEALTH_H
 #define HEALTH_H
-
-void place_foods_and_potions (char **map, int number_of_enemies);
+int locate_positions_consumables(int map_heigth, int map_width, char **map, Consumables consumables[], int number_of_enemies, int nor_size, Non_overlaping_rooms no_overlpg[]);
+void place_foods_and_potions(int map_heigth, int map_width, char** map, Consumables consumables[], int number_of_enemies, int nor_size, Non_overlaping_rooms no_overlpg[]);
 void food_and_potions (char **map, Character* character, Consumables consumables[], char *previous_char);
 
 
