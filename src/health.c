25edfa0a3b6de5029b154a876fb1714676b2a653
daniellpsilva1 <a104086@alunos.uminho.pt>
2023-05-16 17:46:42 +0100

    Experimentations on Food Spawns

--- src/health.c
+++ src/health.c
@@ -12,8 +12,7 @@
 #include "MOBs.h"
 #include "vision.h"
 
-
-/* Function to locate the positions available to put the consumables in the Non overlaping rooms */
+/* Function to locate the positions available to put the consumables INSIDE the Non overlaping rooms */
 void locate_positions_consumables(int map_heigth, int map_width, char **map, Consumables *consumables, int number_of_enemies, int nor_size, Non_overlaping_rooms no_overlpg[]) {
   int consumables_ind = 0;
   int rooms_ind = 1;
@@ -57,51 +56,6 @@ void locate_positions_consumables(int map_heigth, int map_width, char **map, Con
     }
   }
 }
-
-// /* Function to locate the positions available to put the consumables out of the Non overlaping rooms */
-// void locate_positions_consumables_outside(int map_heigth, int map_width, char **map, Consumables *consumables, int number_of_enemies, int nor_size, Non_overlaping_rooms no_overlpg[]) {
-//   int consumables_ind = 0;
-//   int rooms_ind = 1;
-//   int max_per_room = 0;
-//   int offset;
-//   int impossible_location = 0;  /* Max offset from the center of a room */
-
-//   /* Use this number to determine the number of foods/potions in the map */
-//   int number_of_consumables = number_of_enemies / 3;
-
-//   for (; rooms_ind < nor_size; rooms_ind ++)
-//   {
-//     offset = 4;
-//     impossible_location = 0;
-//     max_per_room = 0;
-//     while (consumables_ind < number_of_consumables && impossible_location == 0 && max_per_room < 2)
-//     {
-//       int possible_x = no_overlpg[rooms_ind].midX + (rand () % offset - rand () % offset);
-//       int possible_y = no_overlpg[rooms_ind].midY + (rand () % offset - rand () % offset);
-
-//       if (possible_x < map_width && possible_x > 0 && possible_y < map_heigth && possible_y > 0 && map[possible_y][possible_x] == FLOOR_CHAR)
-//       {
-//         consumables[consumables_ind].x = possible_x;
-//         consumables[consumables_ind].y = possible_y;
-//         max_per_room ++;
-//         consumables_ind ++;
-//         offset += 2;
-
-//         if (consumables_ind % 2 == 0) {
-//           /* Add FOOD_CHAR */
-//           map[possible_y][possible_x] = FOOD_CHAR;
-//         } else {
-//           /* Add POTION_CHAR */
-//           map[possible_y][possible_x] = POTION_CHAR;
-//         }
-//       }
-//       else
-//       {
-//         impossible_location ++;
-//       }
-//     }
-//   }
-// }
 
 /*
 * Function to spawn foods and potions in the map (WITHOUT ROOMS)
