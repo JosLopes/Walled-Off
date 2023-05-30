45be74e6168c934667b97994b12311e0ea10bb3c
Pato-compacto <130843380+Pato-compacto@users.noreply.github.com>
2023-05-30 12:41:54 +0100

    Update mapgen.c

--- src/mapgen.c
+++ src/mapgen.c
@@ -10,10 +10,12 @@
 #include "vision.h"
 #include "MOBs.h"
 
-/*  é responsável por preencher o mapa com o caractere do chão (FLOOR_CHAR). 
-Ela percorre todas as posições do mapa e verifica se o caractere atual não é o caractere da parede (WALL_CHAR) 
-ou o caractere do chão (FLOOR_CHAR). Caso não seja nenhum desses, ela preenche a posição com o caractere %,
-indicando que aquele espaço ainda não foi preenchido. */
+/*  
+a103999 - Ivo Filipe Mendes Vieira
+This function is responsible for filling the map with the floor character (FLOOR_CHAR).
+It iterates through all positions of the map and checks if the current character is not the wall character (WALL_CHAR)
+or the floor character (FLOOR_CHAR). If it is neither of those, it fills the position with the character '%',
+indicating that the space has not been filled yet.  */
 
               
 void fillMap(int map_height, int map_width, char **map) {
@@ -25,7 +27,7 @@ void fillMap(int map_height, int map_width, char **map) {
         }
     }
 
-    /*Preenche as bordas do mapa com paredes*/
+    /*Fills the borders of the map with walls*/
     for (i = 0; i < map_width; i++) {
         map[0][i] = WALL_CHAR;
         map[map_height - 1][i] = WALL_CHAR;
@@ -37,6 +39,12 @@ void fillMap(int map_height, int map_width, char **map) {
     }
 }
 
+/* 
+a103999 - Ivo Filipe Mendes Vieira
+This function is responsible for filling the traveled path map with the undiscovered path character (UNDISCOVERED_PATH_CHAR).
+It iterates through all positions of the map and sets the character to UNDISCOVERED_PATH_CHAR, indicating that the path
+at that position has not been traversed yet. The function also fills the borders of the map with walls. */
+
 void fillTraveledPath(int map_height, int map_width, char traveled_path[][map_width]) {
     int i, j;
 
@@ -45,7 +53,7 @@ void fillTraveledPath(int map_height, int map_width, char traveled_path[][map_wi
           traveled_path[i][j] = UNDISCOVERED_PATH_CHAR;
         }
     }
-    /*Preenche as bordas do mapa com paredes*/
+    
     for (i = 0; i < map_width; i++) {
         traveled_path[0][i] = WALL_CHAR;
         traveled_path[map_height - 1][i] = WALL_CHAR;
@@ -56,103 +64,107 @@ void fillTraveledPath(int map_height, int map_width, char traveled_path[][map_wi
         traveled_path[i][map_width - 1] = WALL_CHAR;
     }
 }
+/* 
+a103999 - Ivo Filipe Mendes Vieira
+This function generates rooms on the game map based on the given parameters. 
+It returns the number of non-overlapping rooms generated. */
 
-/*A função generateRooms recebe como parâmetros o mapa do jogo e uma variável numRooms,
-que indica quantas salas devem ser geradas no mapa.*/
 int generateRooms(int map_height, int map_width, char **map, Room rooms[], int numRooms) {
-    int i, j, k, number_of_non_overlaping_rooms = 0;
+    int i, j, k, number_of_non_overlapping_rooms = 0;
 
-    /*O ciclo for cria as salas aleatoriamente. A cada iteração, uma nova sala é gerada. 
-    O número de iterações é definido pela variável numRooms.*/
+    /* Iterates to create rooms randomly. A new room is generated in each iteration.
+       The number of iterations is determined by the variable numRooms. */
     for (i = 0; i < numRooms; i++) {
-        /*Define as dimensões da sala*/
+        /* Define the dimensions of the room */
         int room_Width = rand() % (MAX_ROOM_WIDTH - MIN_ROOM_WIDTH + 1) + MIN_ROOM_WIDTH;
         int room_Height = rand() % (MAX_ROOM_HEIGHT - MIN_ROOM_HEIGHT + 1) + MIN_ROOM_HEIGHT;
 
-        /* Escolhe uma posição aleatória para a sala*/
+        /* Choose a random position for the room */
         int roomX = rand() % (map_width - room_Width - 1) + 1;
         int roomY = rand() % (map_height - room_Height - 1) + 1;
 
-        /* Verifica se a sala se sobrepõe a outra sala existente*/
+        /* Check if the room overlaps with any existing rooms */
         int overlap = 0;
         for (j = 0; j < i && overlap == 0; j++) {
             if (roomX < rooms[j].x + rooms[j].width &&
                 roomX + room_Width > rooms[j].x &&
                 roomY < rooms[j].y + rooms[j].height &&
                 roomY + room_Height > rooms[j].y) {
-                rooms[i].is_overlaping = 1;
+                rooms[i].is_overlapping = 1;
                 overlap = 1;
             }
         }
 
-        if (overlap == 0){ 
-          rooms[i].is_overlaping = 0;
-          number_of_non_overlaping_rooms ++;
+        if (overlap == 0) {
+            rooms[i].is_overlapping = 0;
+            number_of_non_overlapping_rooms++;
 
-          /*Se não houver sobreposição, adiciona a sala ao mapa*/
-          for (j = roomY; j < roomY + room_Height; j++) {
-              for (k = roomX; k < roomX + room_Width; k++) {
-                  map[j][k] = FLOOR_CHAR;
-                  map[roomY][k] = WALL_CHAR;
-                  map[roomY + room_Height][k] = WALL_CHAR;
-              }
-          map[j][roomX] = WALL_CHAR;
-          map[j][roomX + room_Width] = WALL_CHAR;
-          }
-        } else {
-        /*Se não houver sobreposição, adiciona a sala ao mapa*/
-          for (j = roomY; j < roomY + room_Height; j++) {
-            for (k = roomX; k < roomX + room_Width; k++) {
-              map[j][k] = FLOOR_CHAR;
+            /* If there is no overlap, add the room to the map */
+            for (j = roomY; j < roomY + room_Height; j++) {
+                for (k = roomX; k < roomX + room_Width; k++) {
+                    map[j][k] = FLOOR_CHAR;
+                    map[roomY][k] = WALL_CHAR;
+                    map[roomY + room_Height][k] = WALL_CHAR;
+                }
+                map[j][roomX] = WALL_CHAR;
+                map[j][roomX + room_Width] = WALL_CHAR;
             }
-          }
+        } else {
+            /* If there is overlap, add the room to the map without walls */
+            for (j = roomY; j < roomY + room_Height; j++) {
+                for (k = roomX; k < roomX + room_Width; k++) {
+                    map[j][k] = FLOOR_CHAR;
+                }
+            }
         }
 
-        /* Atualiza colocando aleatóriamente walls */
-
-        /*Adiciona a sala à lista de salas */
+        /* Add the room to the rooms list */
         rooms[i].x = roomX;
         rooms[i].y = roomY;
         rooms[i].width = room_Width;
-        rooms[i].height = room_Height; 
+        rooms[i].height = room_Height;
     }
-    return number_of_non_overlaping_rooms;
+    return number_of_non_overlapping_rooms;
 }
 
-void init_non_overlaping_rooms (Room rooms[], int numRooms, Non_overlaping_rooms no_overlpg[], int nor_size)
-{
-  int room_number = 0, overlaping_list = 0;
+/* 
+a103999 - Ivo Filipe Mendes Vieira
+This function initializes the list of non-overlapping rooms by extracting the midpoint coordinates of 
+each non-overlapping room from the generated rooms array.
+The extracted coordinates are stored in the no_overlpg array. */
 
-  for (; room_number < numRooms && overlaping_list < nor_size; room_number++)
-  {
-    if (rooms[room_number].is_overlaping == 0)
-    {
-      no_overlpg[overlaping_list].midX = (rooms[room_number].width >> 1) + rooms[room_number].x;
-      no_overlpg[overlaping_list].midY = (rooms[room_number].height >> 1) + rooms[room_number].y;
-      overlaping_list ++;
+void init_non_overlapping_rooms(Room rooms[], int numRooms, Non_overlapping_rooms no_overlpg[], int nor_size) {
+  int room_number = 0, overlapping_list = 0;
+
+  for (; room_number < numRooms && overlapping_list < nor_size; room_number++) {
+    if (rooms[room_number].is_overlapping == 0) {
+      no_overlpg[overlapping_list].midX = (rooms[room_number].width >> 1) + rooms[room_number].x;
+      no_overlpg[overlapping_list].midY = (rooms[room_number].height >> 1) + rooms[room_number].y;
+      overlapping_list++;
     }
   }
 }
+/* 
+a103999 - Ivo Filipe Mendes Vieira
+This function generates corridors between non-overlapping rooms on the game map. It takes the map, 
+the array of non-overlapping rooms, and the number of non-overlapping rooms as parameters. */
 
-void generateCorridors (char **map, Non_overlaping_rooms *no_overlpg, int nor_size) 
-{
+void generateCorridors(char **map, Non_overlapping_rooms *no_overlpg, int nor_size) {
   int bridge_ind = 0, temp;
   Vector vector;
-  
-  for (; bridge_ind + 1 < nor_size; bridge_ind++)
-  {
+
+  for (; bridge_ind + 1 < nor_size; bridge_ind++) {
     vector.startingX = no_overlpg[bridge_ind].midX;
-    int endingX = no_overlpg[bridge_ind+1].midX;
+    int endingX = no_overlpg[bridge_ind + 1].midX;
     vector.startingY = no_overlpg[bridge_ind].midY;
-    int endingY = no_overlpg[bridge_ind+1].midY;
-    
-    // calcula a direção do corredor
+    int endingY = no_overlpg[bridge_ind + 1].midY;
+
+    // Calculate the direction of the corridor
     int directionX = endingX - vector.startingX;
     int directionY = endingY - vector.startingY;
 
-    /*se o corredor estiver na direção negativa em X, inverte os pontos inicial e final*/
-    if (directionX < 0 && directionY < 0) 
-    {
+    /* If the corridor is in the negative X direction, reverse the starting and ending points */
+    if (directionX < 0 && directionY < 0) {
       temp = vector.startingX;
       vector.startingX = endingX;
       endingX = temp;
@@ -162,65 +174,55 @@ void generateCorridors (char **map, Non_overlaping_rooms *no_overlpg, int nor_si
       endingY = temp;
     }
 
-    if ((directionX > 0 && directionY > 0) || 
-        (directionX < 0 && directionY < 0))
-    {
-      // percorre o corredor, desenhando o chão (FLOOR_CHAR) em cada posição
+    if ((directionX > 0 && directionY > 0) || (directionX < 0 && directionY < 0)) {
+      // Traverse the corridor, drawing the floor (FLOOR_CHAR) at each position
       for (; vector.startingX <= endingX; vector.startingX++) {
         if (map[vector.startingY][vector.startingX] == WATER_CHAR ||
-            map[vector.startingY][vector.startingX] == WALL_CHAR)
-        {
+            map[vector.startingY][vector.startingX] == WALL_CHAR) {
           map[vector.startingY][vector.startingX] = FLOOR_CHAR;
         }
       }
-    
+
       for (; vector.startingY <= endingY; vector.startingY++) {
         if (map[vector.startingY][vector.startingX] == WATER_CHAR ||
-            map[vector.startingY][vector.startingX] == WALL_CHAR)
-        {
+            map[vector.startingY][vector.startingX] == WALL_CHAR) {
           map[vector.startingY][vector.startingX] = FLOOR_CHAR;
         }
       }
-    } 
-    else if ((directionX < 0 || directionX == 0) && (directionY > 0 || directionY == 0)) 
-    {
+    } else if ((directionX < 0 || directionX == 0) && (directionY > 0 || directionY == 0)) {
       for (; vector.startingX > endingX; vector.startingX--) {
         if (map[vector.startingY][vector.startingX] == WATER_CHAR ||
-            map[vector.startingY][vector.startingX] == WALL_CHAR)
-        {
-        map[vector.startingY][vector.startingX] = FLOOR_CHAR;
+            map[vector.startingY][vector.startingX] == WALL_CHAR) {
+          map[vector.startingY][vector.startingX] = FLOOR_CHAR;
         }
       }
-    
+
       for (; vector.startingY <= endingY; vector.startingY++) {
         if (map[vector.startingY][vector.startingX] == WATER_CHAR ||
-            map[vector.startingY][vector.startingX] == WALL_CHAR)
-        {
-        map[vector.startingY][vector.startingX] = FLOOR_CHAR;
+            map[vector.startingY][vector.startingX] == WALL_CHAR) {
+          map[vector.startingY][vector.startingX] = FLOOR_CHAR;
         }
       }
-    }
-    else if ((directionX > 0 || directionX == 0) && (directionY < 0 || directionY == 0))
-    {
+    } else if ((directionX > 0 || directionX == 0) && (directionY < 0 || directionY == 0)) {
       for (; vector.startingX <= endingX; vector.startingX++) {
         if (map[vector.startingY][vector.startingX] == WATER_CHAR ||
-            map[vector.startingY][vector.startingX] == WALL_CHAR)
-        {
-        map[vector.startingY][vector.startingX] = FLOOR_CHAR;
+            map[vector.startingY][vector.startingX] == WALL_CHAR) {
+          map[vector.startingY][vector.startingX] = FLOOR_CHAR;
         }
       }
-    
+
       for (; vector.startingY > endingY; vector.startingY--) {
         if (map[vector.startingY][vector.startingX] == WATER_CHAR ||
-            map[vector.startingY][vector.startingX] == WALL_CHAR)
-        {
-        map[vector.startingY][vector.startingX] = FLOOR_CHAR;
+            map[vector.startingY][vector.startingX] == WALL_CHAR) {
+          map[vector.startingY][vector.startingX] = FLOOR_CHAR;
         }
       }
     }
   }
 }
-
+/*
+a103999 - Ivo Filipe Mendes Vieira
+places the player on the map*/
 void place_player(Non_overlaping_rooms room, char **map, Character *character) {
     character -> x = room.midX;
     character -> y = room.midY;
