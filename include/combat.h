7df87d086f4a58c64bd0245c9fb1b5063a915fb4
Pato-compacto <130843380+Pato-compacto@users.noreply.github.com>
2023-06-05 21:20:45 +0100

    Update combat.h

--- include/combat.h
+++ include/combat.h
@@ -3,16 +3,7 @@
 #ifndef COMBAT_H
 #define COMBAT_h
 
-Weapon *choose_weapon(Character *character);
-float calculate_enemy_damage(Character *character, Awake *is_awake);
-void character_take_damage(Character *character, Enemy *enemy, float total_damage);
-void enemy_attack(Character *character, Enemy *enemy, Awake *is_awake);
-void enemy_sees_character(Character *character, Enemy *enemy);
-void remove_dead_enemy(int index, Awake *is_awake, Enemy *enemies, char **map);
-void enemy_take_damage(Character *character, Awake *is_awake, char **map);
-void handle_attack_input(Character *character, Enemy *enemy, Awake *is_awake, char **map);
-void handle_special_power_input(Character *character);
-void update_game(Character *character);
+
 void attack(Character *character, Enemy *enemy, Awake *is_awake, char **map, int ch);
 
 
